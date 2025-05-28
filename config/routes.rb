b4ef7c61e0b70b26467d76f6c2d0e5ee24b4e68d
Лилия Терещенкова <k.9602585349@gmail.com>
2025-05-28 18:02:34 +0300

    feat: install webpack

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  match '/about' => 'application#about'
-  match '/author' => 'application#author'
+  # match '/about' => 'application#about'
+  # match '/author' => 'application#author'
 end
