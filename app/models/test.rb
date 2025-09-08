92045ac796876ef496faebf51fbadc06735acc2e
Lilya-te <k.9602585349@gmail.com>
2025-09-08 20:06:30 +0300

    methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.tests_by_category(category_title:)
+    category_ids = Category.where(title: category_title).pluck(:id)
+    Test.where(category_id: category_ids).pluck(:title).sort.reverse
+  end
 end
