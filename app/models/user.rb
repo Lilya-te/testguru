1a9c5c51c64a77c24981f49e8fe97997d1d59616
Lilya-te <k.9602585349@gmail.com>
2025-09-09 20:09:42 +0300

    Revert "methods"
    This reverts commit 92045ac796876ef496faebf51fbadc06735acc2e.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,2 @@
 class User < ApplicationRecord
-  def test_list(level)
-    question_ids = Answer.where(user_id: self.id).map(&:question_id)
-    test_ids = Question.where(id: question_ids).map(&:test_id)
-    Test.where(id: test_ids, level: level)
-  end
 end
