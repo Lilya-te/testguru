f7bbda5f056d5487aad7bc3fbc38f0d0b09ca5c9
Lilya-te <k.9602585349@gmail.com>
2025-09-09 20:09:58 +0300

    Revert "seeds"
    This reverts commit 0d31ab7e584d003cb03f8170d0ff58aaad7193eb.

--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,8 @@ ActiveRecord::Schema.define(version: 2025_06_25_113125) do
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
     t.integer "user_id", null: false
-    t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
@@ -48,13 +46,12 @@ ActiveRecord::Schema.define(version: 2025_06_25_113125) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "CreateUsers", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["CreateUsers"], name: "index_users_on_CreateUsers", unique: true
   end
 
-  add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-Category.create([{ title: "Math" }, { title: "Programming" }, { title: "English" }])
-User.create([{ name: "John" }, { name: "Paul" }, { name: "George" }, { name: "Ringo" }])
-Test.create([{ title: "Areas", level: 1, category_id: Category.first.id, author_id: User.first.id },
-             { title: "Ruby", level: 2, category_id: Category.second.id, author_id: User.second.id },
-             { title: "Tenses", level: 3, category_id: Category.third.id, author_id: User.third.id }])
-Question.create([{ body: "Area of a triangle?", test_id: Test.first.id },
-                 { body: "Which datatypes do you know?", test_id: Test.second.id },
-                 { body: "Which feature tenses do you know?", test_id: Test.third.id }])
-Answer.create([{ user_id: User.first.id, question_id: Question.first.id, correct: true },
-               { user_id: User.last.id, question_id: Question.second.id, correct: false }])
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
