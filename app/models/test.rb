e67995b3ba9e73f2e5e3c6eeb4be330fea4db7c0
Lilya-te <k.9602585349@gmail.com>
2025-09-10 13:17:50 +0300

    methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  def self.tests_by_category(category_title:)
+    self.joins(:category).where(categories: {title: category_title}).pluck(:title).sort.reverse
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_tests
+
+  def my_tests(level:)
+    self.user_tests.map(&:test).select{_1.level == level}
+  end
 end
